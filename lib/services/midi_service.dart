08b4604350c285d16a249f0b88b50d79f322880a
DiamondHorizon <jakelego88@gmail.com>
2025-09-28 13:23:12 -0400

    Fix listening for multiple notes

--- lib/services/midi_service.dart
+++ lib/services/midi_service.dart
@@ -46,6 +46,25 @@ class MidiService {
     _requiredNotes = requiredNotes;
     _pressedNotes.clear();
     _chordCompleter = Completer<void>();
+
+    late StreamSubscription<MidiPacket> subscription;
+
+    subscription = command.onMidiDataReceived!.listen((MidiPacket packet) {
+      final data = packet.data;
+      if (data.isNotEmpty && (data[0] & 0xF0) == 0x90 && data[2] > 0) { // NoteOn with velocity
+        final playedNote = data[1];
+        _pressedNotes.add(playedNote);
+
+        if (_pressedNotes.containsAll(_requiredNotes)) {
+          subscription.cancel();
+          _chordCompleter!.complete();
+          _chordCompleter = null;
+          _pressedNotes.clear();
+          _requiredNotes.clear();
+        }
+      }
+    });
+
     return _chordCompleter!.future;
   }
 }
